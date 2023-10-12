bf438f5cf10c1692e205a8802bd00508a49ac222
fjakuro <fjakuro@gmail.com>
2023-10-12 23:53:25 +0900

    fix part of part1

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: yiwama <yiwama@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 13:45:13 by yiwama            #+#    #+#             */
-/*   Updated: 2023/09/25 13:56:54 by yiwama           ###   ########.fr       */
+/*   Updated: 2023/10/12 01:05:23 by yiwama           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,29 @@ void	*ft_memchr(const void *s, int c, size_t n)
 			return ((void *)&str[i]);
 		i++;
 	}
-	if (str[i] == c)
+	if (i < n && str[i] == c)
 		return ((void *)&str[i]);
 	return (0);
 }
 
-// #include <stdio.h>
-// #include <string.h>
-// int	main(void)
-// {
-// 	char	str0[] = "abcdefabcdef";
-// 	printf("%p\n", memchr(str0, 'e', 20));
-// 	printf("%p\n", ft_memchr(str0, 'e', 20));
+#include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	char	str0[] = "abcdefabcdef";
+	printf("%p, %s\n", memchr(str0, 'e', 20), (char *)memchr(str0, 'e', 20));
+	printf("%p, %s\n", ft_memchr(str0, 'e', 20), (char *)ft_memchr(str0, 'e', 20));
 
-// 	printf("%p\n", memchr(str0, 'e', 3));
-// 	printf("%p\n", ft_memchr(str0, 'e', 3));
+	printf("%p, %s\n", memchr(str0, 'e', 3), (unsigned char *)memchr(str0, 'e', 3));
+	printf("%p, %s\n", ft_memchr(str0, 'e', 3), (unsigned char *)ft_memchr(str0, 'e', 3));
 
-// 	printf("%p\n", memchr(str0, '\0', 20));
-// 	printf("%p\n", ft_memchr(str0, '\0', 20));
-// }
+	printf("%p, %s\n", memchr(str0, '\0', 20), (unsigned char *)memchr(str0, '\0', 20));
+	printf("%p, %s\n", ft_memchr(str0, '\0', 20), (unsigned char *)ft_memchr(str0, '\0', 20));
+
+	char	test[] = {0, 1, 2, 3, 4, 5};
+	// printf("%p\n", memchr(test, 0, 0));
+	// printf("%p\n", ft_memchr(test, 0, 0));
+
+	printf("%p\n", memchr(test, 2, 3));
+	printf("%p\n", ft_memchr(test, 2, 3));
+}
